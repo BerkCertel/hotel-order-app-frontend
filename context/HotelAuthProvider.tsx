860d4cd696a534ea9bd060dfd340cf8a91dbba2e
Berk Certel <m.berkcertel@outlook.com>
2025-09-16 21:24:06 +0300

    auth edit

--- context/HotelAuthProvider.tsx
+++ context/HotelAuthProvider.tsx
@@ -26,8 +26,8 @@ export const HotelAuthProvider: React.FC<{ children: React.ReactNode }> = ({
   return (
     <HotelAuthContext.Provider value={{ isAuthenticated }}>
       <HotelAuthModal
-        // open={!isAuthenticated || !orderUser || !orderUser.roomNumber}
-        open={isAuthenticated}
+        open={!isAuthenticated || !orderUser || !orderUser.roomNumber}
+        // open={isAuthenticated}
         onSuccess={(roomNumber, name) => {
           dispatch(setOrderUser({ roomNumber, name })); // isim de redux’a kaydolur
           setAuthenticated(true);
